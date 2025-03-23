700b40cd9703e1bf8b287da7577b4a1f741c1a3d
surya teja <suryateja.ravuri@apxor.com>
2025-03-23 15:42:34 +0530

    feat: delete property handler added

---
+++ services/properties/src/handlers/deletePropertyHandler.js
@@ -0,0 +1,25 @@
+const { connectToDatabase } = require('../../../auth/src/lib/mongodb');
+const response = require('../../../auth/src/lib/response');
+
+const handler = async (event) => {
+    try {
+        const propertyId = event.pathParameters['id'];
+
+        // Connect to MongoDB
+        const db = await connectToDatabase();
+
+        // Create propertylisting
+        await db.collection('properties').deleteOne({ _id: propertyId });
+
+        return response.success({
+            message: 'Property deleted successfully'
+        });
+    } catch (error) {
+        console.error('delete property error:', error);
+        return response.error('Internal server error', 500);
+    }
+};
+
+module.exports = {
+    handler,
+};
