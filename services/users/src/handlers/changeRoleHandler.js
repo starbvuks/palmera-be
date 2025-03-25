044dea9f636bc2a30cb572b4676a395838e8c73e
surya teja <suryateja.ravuri@apxor.com>
2025-03-25 21:50:16 +0530

    fix: changes to role change handler

--- services/users/src/handlers/changeRoleHandler.js
+++ services/users/src/handlers/changeRoleHandler.js
@@ -1,9 +1,10 @@
 const { connectToDatabase } = require('../../../auth/src/lib/mongodb');
 const response = require('../../../auth/src/lib/response');
 
-const handler = async(event) => {
+const handler = async (event) => {
     try {
         const userId = event.pathParameters['id'];
+        const role = event.queryStringParameters['role'] || 'host';
 
         // Connect to MongoDB
         const db = await connectToDatabase();
@@ -14,14 +15,20 @@ const handler = async(event) => {
             return response.error('User not found', 404);
         }
 
+        const query = role == "host" ? {
+            "roles.isHost": true
+        } : {
+            "roles.isHost": false
+        }
+        if (role == "host" && (!user.hostDetails || !user.hostDetails.verification || !user.hostDetails.verification.status || user.hostDetails.verification.status != 'verified')) {
+            return response.error('Host verification pending', 400);
+        }
         // Update user
         await db.collection('users').updateOne({ _id: userId }, {
-            $set: {
-                "roles.isHost": true
-            }
+            $set: query
         });
 
-        return response.success({ message: 'User role updated successfully' });
+        return response.success({ message: 'User role updated to ' + role + ' successfully' });
     } catch (error) {
         console.error('Update user role error:', error);
         return response.error('Internal server error', 500);
