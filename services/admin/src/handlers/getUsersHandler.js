87256f2b19b1e83f122a760565684a731bc43c82
ravuri krishna murthy <srisuryateja.r.official@gmail.com>
2025-05-05 01:42:38 +0530

    feat: adding admin module

---
+++ services/admin/src/handlers/getUsersHandler.js
@@ -0,0 +1,59 @@
+const { connectToDatabase } = require('../../../auth/src/lib/mongodb');
+const response = require('../../../auth/src/lib/response');
+const Joi = require('joi');
+
+// Schema for validating query parameters
+const searchSchema = Joi.object({
+    role: Joi.string().valid('admin', 'host', 'guest','superhost'),
+    status: Joi.string().valid('active', 'suspended', 'deleted'),
+    registered_from: Joi.date(),
+    registered_to: Joi.date(),
+});
+
+const handler = async (event) => {
+    try {
+        let requestFilters = event.queryStringParameters || {};
+        // Validate input
+        const { error } = searchSchema.validate(requestFilters);
+        if (error) {
+            return response.error(error.details[0].message, 400);
+        }
+
+        filters = searchSchema.validate(requestFilters).value;
+        let query = {};
+        if (filters.role) {
+            if (filters.role === 'host') {
+                query["roles.isHost"] = true;  
+            }
+            if (filters.role === 'superhost') {
+                query["roles.isSuperhost"] = true;  
+            }
+            if (filters.role === 'admin') {
+                query["roles.isAdmin"] = true;  
+            }
+            if (filters.role === 'guest') {
+                query["roles.isHost"] = false;  
+            }
+        }
+        if (filters.status) query["accountStatus.status"] = filters.status;
+        if (filters.registered_from || filters.registered_to) {
+            query["accountStatus.createdAt"] = {};
+            if (filters.registered_from) query["accountStatus.createdAt"]["$gte"] =new Date(filters.registered_from);
+            if (filters.registered_to) query["accountStatus.createdAt"]["$lte"]= new Date(filters.registered_to)};
+        
+        // Connect to MongoDB
+        const db = await connectToDatabase();
+        // Check if property exists
+        console.log('filters:', query);
+        const users = await db.collection('users').find(query).toArray();
+
+        return response.success({ users, count: users.length }, 200);
+    } catch (error) {
+        console.error('Get users error:', error);
+        return response.error('Internal server error', 500);
+    }
+};
+
+module.exports = {
+    handler,
+};
---
+++ services/admin/src/handlers/updateUserHandler.js
@@ -0,0 +1,44 @@
+const { connectToDatabase } = require('../../../auth/src/lib/mongodb');
+const response = require('../../../auth/src/lib/response');
+const {
+    userSchema
+} = require('../../../users/src/lib/userDAL.js');
+
+const handler = async (event) => {
+    try {
+        const userId = event.pathParameters['id'];
+        const updateData = JSON.parse(event.body);
+
+        // Validate input
+        const { error } = userSchema.validate(updateData);
+        if (error) {
+            console.error(error);
+            return response.error(error.details[0].message, 400);
+        }
+
+        // Connect to MongoDB
+        const db = await connectToDatabase();
+
+        // Check if user exists
+        const user = await db.collection('users').findOne({ _id: userId });
+        if (!user) {
+            return response.error('User not found', 404);
+        }
+        // Update user
+        await db.collection('users').updateOne({ _id: userId }, {
+            $set: {
+                ...updateData,
+                "accountStatus.updatedAt": new Date()
+            }
+        });
+
+        return response.success({ message: 'User updated by admin successfully' });
+    } catch (error) {
+        console.error('Update user by admin error:', error);
+        return response.error('Internal server error', 500);
+    }
+};
+
+module.exports = {
+    handler,
+};
